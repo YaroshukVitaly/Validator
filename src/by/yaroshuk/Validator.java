0f44e00c17ad882de86f79132de9a32a0e85bef1
Vitalya <yaroshukvi@gmail.com>
2018-08-20 00:20:21 +0300

    Исправлен

--- src/by/yaroshuk/Validator.java
+++ src/by/yaroshuk/Validator.java
@@ -5,92 +5,92 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-    public static boolean phoneNum (String phoneNum){
+    public static boolean checkPhoneNum (String phoneNum){
         Pattern pattern = Pattern.compile("^((\\+?375)([0-9]){9})$");
         Matcher matcher = pattern.matcher(phoneNum);
         if (matcher. matches()){
-            System.out.println("Phone number is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("Phone number is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean email (String email){
+    public static boolean checkEmail (String email){
         Pattern pattern = Pattern.compile("^[a-zA-Z0-9]{1,}"+"((\\.|\\_|-{0,1})[a-zA-Z0-9]{1,})*"+"@"
                 +"[a-zA-Z0-9]{1,}"+"((\\.|\\_|-{0,1})[a-zA-Z0-9]{1,})*"+"\\.[a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(email);
         if (matcher. matches()){
-            System.out.println("Email is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("Email is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean name (String name){
+    public static boolean checkName (String name){
         Pattern pattern = Pattern.compile("^[А-Яа-я]{4,16}$");
         Matcher matcher = pattern.matcher(name);
         if (matcher. matches()){
-            System.out.println("Name is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("Name is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean password (String password){
+    public static boolean checkPassword (String password){
         Pattern pattern = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9@#$%]).{8,16}$");
         Matcher matcher = pattern.matcher(password);
         if (matcher. matches()){
-            System.out.println("Password is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("Password is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean numPassport (String numPassport){
+    public static boolean checkNumPassport (String numPassport){
         Pattern pattern = Pattern.compile("^[0-9]{1,7}$");
         Matcher matcher = pattern.matcher(numPassport);
         if (matcher. matches()){
-            System.out.println("NumPassport is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("NumPassport is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean whoGetPasport (String whoGetPasport){
+    public static boolean checkWhoGetPasport (String whoGetPasport){
         Pattern pattern = Pattern.compile("^[А-яа-я\\s]{1,60}$");
         Matcher matcher = pattern.matcher(whoGetPasport);
         if (matcher. matches()){
-            System.out.println("WhoGetPasport is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("WhoGetPasport is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean whoGetDriveDoc (String whoGetDriveDoc){
+    public static boolean checkWhoGetDriveDoc (String whoGetDriveDoc){
         Pattern pattern = Pattern.compile("^[А-яа-я\\s]{1,60}$");
         Matcher matcher = pattern.matcher(whoGetDriveDoc);
         if (matcher. matches()){
-            System.out.println("WhoGetDriveDoc is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("WhoGetDriveDoc is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
-    public static boolean birthPlace (String birthPlace){
+    public static boolean checkBirthPlace (String birthPlace){
         Pattern pattern = Pattern.compile("^[А-яа-я\\s]{1,60}$");
         Matcher matcher = pattern.matcher(birthPlace);
         if (matcher. matches()){
-            System.out.println("BirthPlace is correct");
+            System.out.println("is correct");
             return true;
         } else{
-            System.out.println("BirthPlace is incorrect");
+            System.out.println("is incorrect");
             return false;
         }
     }
